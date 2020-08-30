ec4046bb943472569e209f6f9a658d197818bc36
Yuqing <Yuqingbruce@qq.com>
2020-08-30 18:29:49 +1200

    Start up

---
+++ Job Broker Online/Job Broker Online/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Job_Broker_Online.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+    }
+}
---
+++ Job Broker Online/Job Broker Online/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Job_Broker_Online.MvcApplication" Language="C#" %>
